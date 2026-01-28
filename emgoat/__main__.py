935e0e0b212b34d327a4d8c3de6a4e8c8f31c353
Fenglai Liu <fliu08@stjude.org>
2026-01-28 17:26:04 -0600

    update the __main__.py, add in LSF json result output

--- emgoat/__main__.py
+++ emgoat/__main__.py
@@ -5,7 +5,7 @@ import sys
 import argparse
 
 import emgoat
-
+from emgoat.cluster.lsf import Cluster as LSFCluster
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -13,11 +13,24 @@ here = os.path.abspath(os.path.dirname(__file__))
 if __name__ == '__main__':
     p = argparse.ArgumentParser(prog='emgoat')
 
+    # parameters for the action 
+    p.add_argument('--generate_cluster_usage_data', action='store_true',
+                   help='Generating json format of cluster usage data')
+
+    # form the args
+    args = p.parse_args()
+
+    # for LSF cluster whether we generate the cluster usage data?
+    if args.generate_cluster_usage_data:
+        LSFCluster().generate_json_results()
+
+    """
+    Fenglai: currently disable the code below
     p.add_argument("template")
     p.add_argument('--debug', '-d', action="store_true",
                    help="Debug mode: just generate the template script"
                         "and print launching command. ")
 
-    args = p.parse_args()
 
     emgoat.EMGoat(args.template, debug=args.debug).launch_job()
+    """
